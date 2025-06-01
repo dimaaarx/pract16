247206ab5fa25a74e931e85ad75edc7bdb3b316e
dimaaarx <koptevdima1107@gmail.com>
2025-06-02 01:35:21 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,38 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        SettingsManager.loadSettings();
+        while (true) {
+            int choice = showMenu();
+            switch (choice) {
+                case 1:
+                    Game.start();
+                    break;
+                case 2:
+                    SettingsManager.changeSettings();
+                    break;
+                case 3:
+                    SettingsManager.showStatistics();
+                    break;
+                case 0:
+                    System.out.println("Вихід з гри");
+                    return;
+                default:
+                    System.out.println("Невірний вибір. Спробуйте ще раз.");
+            }
         }
+    }
+
+    private static int showMenu() {
+        System.out.println("=== Головне меню ===");
+        System.out.println("1. Почати гру");
+        System.out.println("2. Налаштування");
+        System.out.println("3. Показати статистику");
+        System.out.println("0. Вихід");
+        System.out.print("Ваш вибір: ");
+        return scanner.nextInt();
     }
 }
